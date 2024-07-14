7e47f72859c15b5aee8427744c5329ccfb880c17
onweg <Innokentiydiscord@yandex.ru>
2024-07-14 19:41:52 +0300

    спринт 1 2.4.2

--- 01 Основные компоненты C++/01 Спринт 1/02 Базовые алгоритмы/04 Алгоритм accumulate/Задание 2/src/main.cpp
+++ 01 Основные компоненты C++/01 Спринт 1/02 Базовые алгоритмы/04 Алгоритм accumulate/Задание 2/src/main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <numeric>
+
+using namespace std;
+
 string AddStopWords(const string& query, const set<string>& stop_words) {
-    // ...
+    return accumulate(stop_words.begin(), stop_words.end(), query, [](const string& a, const string& b){return a + " -" + b;});
+}
+
+int main() {
+    set<string> stop_words = {"of"s, "in"s};
+    string query = "some tasty oranges"s;
+    cout << AddStopWords(query, stop_words) << endl;
 }
