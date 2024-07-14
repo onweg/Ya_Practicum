06eac6fcb6cfa1b12b13b235d36d6f300514e1e1
onweg <Innokentiydiscord@yandex.ru>
2024-07-14 16:58:06 +0300

    спринт 1 2.2.2

--- 01 Основные компоненты C++/01 Спринт 1/02 Базовые алгоритмы/02 Алгоритмы count и count_if/Задание 2/src/main.cpp
+++ 01 Основные компоненты C++/01 Спринт 1/02 Базовые алгоритмы/02 Алгоритмы count и count_if/Задание 2/src/main.cpp
@@ -5,26 +5,34 @@
 
 using namespace std;
 
-bool comp(string s) {
+/*
+bool comp(const string& s) {
     if (s.size())
         return s[0] == 'a' || s[0] == 'A';
     return false;
 }
+*/
 
 int CountStartsWithA(const vector<string>& xs) {
     // посчитайте число строк, начинающихся на букву A
-    return count_if(xs.begin(), xs.end(), comp);
-//    int count = 0;
-//    for (string s : xs) {
-//        if (s.size()) {
-//            count += (s[0] == 'a' || s[0] == 'A');
-//        }
-//    }
-//    return count;
+    /*
+    int count = 0;
+    for (string s : xs) {
+        if (s.size()) {
+            count += (s[0] == 'a' || s[0] == 'A');
+        }
+    }
+    return count;
+    */
+    
+    //return count_if(xs.begin(), xs.end(), comp);
+    
+    char a = 'a', A = 'A';
+    return count_if(xs.begin(), xs.end(), [a, A](const string& s) { return s.size() > 0 && (s[0] == a || s[0] == A); });
 }
 
 int main() {
     // не меняйте тело main
-    cout << CountStartsWithA({"And", "another", "one", "gone", "another", "one", "bites", "the", "dust", ""});
+    cout << CountStartsWithA({"And"s, "another"s, "one"s, "gone"s, "another"s, "one"s, "bites"s, "the"s, "dust"s, ""s}) << endl;
 		return 0;
 } 
