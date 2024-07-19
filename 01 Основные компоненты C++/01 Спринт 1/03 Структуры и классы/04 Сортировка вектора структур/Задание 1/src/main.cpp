7d3327e7240e4f82e0a5a154a4b1e25523fd0edf
onweg <Innokentiydiscord@yandex.ru>
2024-07-19 14:52:09 +0300

    спринт 1 3.4.1

--- 01 Основные компоненты C++/01 Спринт 1/03 Структуры и классы/04 Сортировка вектора структур/Задание 1/src/main.cpp
+++ 01 Основные компоненты C++/01 Спринт 1/03 Структуры и классы/04 Сортировка вектора структур/Задание 1/src/main.cpp
@@ -1,13 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 struct Document {
 	int id;
 	int relevance;
 };
 
-/* возвращаемое значение */  HasDocumentGreaterRelevance(/* параметры функции */) {
-		return /* условие, при котором первый объект будет в векторе раньше второго */;
+bool HasDocumentGreaterRelevance(Document lhs, Document rhs) {
+		return lhs.relevance < rhs.relevance;
 }
 
 int main() {
-
+    vector<Document> document = {{1, 12}, {2, 10}, {3, 11}};
+    sort(document.begin(), document.end(), HasDocumentGreaterRelevance);
+    for (auto i : document) {
+        cout << i.id << ' ' << i.relevance << endl;
+    }
     return 0;
 }
