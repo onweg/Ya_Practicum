ed3dd9293465863d846e96888823086232f05fc9
onweg <Innokentiydiscord@yandex.ru>
2024-08-06 18:30:52 +0300

    спринт 1 7.6.1

--- 01 Основные компоненты C++/01 Спринт 1/07 Числовые типы/06 Техника безопасности/Задание 1/src/main.cpp
+++ 01 Основные компоненты C++/01 Спринт 1/07 Числовые типы/06 Техника безопасности/Задание 1/src/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,6 +12,13 @@ struct Document {
 
 void PrintDocuments(vector<Document> documents, size_t skip_start, size_t skip_finish) {
 		// отсортировать документы и вывести не все
+    if (documents.empty()) return 0;
+    sort(documents.begin(), documents.end(), [](const Document& a, const Document& b){return a.rating > b.rating;});
+    const auto size = documents.size();
+    if (skip_finish > size) skip_finish = size;
+    for (auto i = skip_start; i < size - skip_finish; i++) {
+        cout << "{ id = "s << documents[i].id << ", rating = "s << documents[i].rating << " }" << endl;
+    }
 }
 
 int main() {
