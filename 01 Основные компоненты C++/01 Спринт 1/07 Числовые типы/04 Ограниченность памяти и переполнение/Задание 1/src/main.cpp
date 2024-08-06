4e271be40eba862bdebff09f393d4b7660831d96
onweg <Innokentiydiscord@yandex.ru>
2024-08-06 16:38:23 +0300

    спринт 1 7.4.1

--- 01 Основные компоненты C++/01 Спринт 1/07 Числовые типы/04 Ограниченность памяти и переполнение/Задание 1/src/main.cpp
+++ 01 Основные компоненты C++/01 Спринт 1/07 Числовые типы/04 Ограниченность памяти и переполнение/Задание 1/src/main.cpp
@@ -5,8 +5,13 @@
 using namespace std;
 
 int main() {
-    const auto min = // минимальное значение типа int64_t
-    const auto max = // максимальное значение типа uint64_t
+    const auto min = numeric_limits<int64_t>::min(); // минимальное значение типа int64_t
+    const auto max = numeric_limits<uint64_t>::max(); // максимальное значение типа uint64_t
+    cout << min << endl;
+    cout << max << endl;
+    cout << min + max << endl;
+    cout << 2 * min << endl;
+    cout << 2 * max << endl;
 
     // выведите 5 чисел
 }
