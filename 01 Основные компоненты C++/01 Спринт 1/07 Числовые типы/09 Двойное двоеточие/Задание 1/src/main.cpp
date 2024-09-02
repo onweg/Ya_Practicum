142bda3fabad139e5eead69d5eac7dc53b9c8d60
onweg <Innokentiydiscord@yandex.ru>
2024-09-02 21:51:15 +0300

    спринт 1 7.9.1 НЕПРОВЕРЕННЫЙ

--- 01 Основные компоненты C++/01 Спринт 1/07 Числовые типы/09 Двойное двоеточие/Задание 1/src/main.cpp
+++ 01 Основные компоненты C++/01 Спринт 1/07 Числовые типы/09 Двойное двоеточие/Задание 1/src/main.cpp
@@ -2,18 +2,16 @@
 
 using namespace std;
 
-enum class DocumentStatus {
-    ACTUAL,
-    IRRELEVANT,
-    BANNED,
-    REMOVED,
-};
-
 class SearchServer {
 public:
-    // перенесите сюда DocumentStatus
+    enum class DocumentStatus {
+        ACTUAL,
+        IRRELEVANT,
+        BANNED,
+        REMOVED,
+    };
 };
 
 int main() {
-    // выведите численное значение статуса BANNED
+    cout << static_cast<int>(SearchServer::DocumentStatus::BANNED);
 }
