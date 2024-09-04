ef4c71ca7dbdd528b4dd2145b4dadc33b61fdbb8
onweg <Innokentiydiscord@yandex.ru>
2024-09-04 18:05:44 +0300

    спринт 1 8.7.1 НЕПРОВЕРЕННЫЙ

--- 01 Основные компоненты C++/01 Спринт 1/08 Пары и кортежи/07 Вещественные числа и задача о задачах/Задание 1/src/main.cpp
+++ 01 Основные компоненты C++/01 Спринт 1/08 Пары и кортежи/07 Вещественные числа и задача о задачах/Задание 1/src/main.cpp
@@ -86,7 +86,8 @@ public:
         
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
-                 return lhs.relevance > rhs.relevance;
+                return lhs.relevance > rhs.relevance ||
+                 ((abs(lhs.relevance - rhs.relevance) < 1e-6) && lhs.rating > rhs.rating);
              });
         if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
             matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
@@ -238,15 +239,12 @@ private:
     }
 };
 
-void PrintMatchDocumentResult(int document_id, const vector<string>& words, DocumentStatus status) {
+void PrintDocument(const Document& document) {
     cout << "{ "s
-         << "document_id = "s << document_id << ", "s
-         << "status = "s << static_cast<int>(status) << ", "s
-         << "words ="s;
-    for (const string& word : words) {
-        cout << ' ' << word;
-    }
-    cout << "}"s << endl;
+         << "document_id = "s << document.id << ", "s
+         << "relevance = "s << document.relevance << ", "s
+         << "rating = "s << document.rating
+         << " }"s << endl;
 }
 
 int main() {
@@ -256,11 +254,8 @@ int main() {
     search_server.AddDocument(0, "белый кот и модный ошейник"s,        DocumentStatus::ACTUAL, {8, -3});
     search_server.AddDocument(1, "пушистый кот пушистый хвост"s,       DocumentStatus::ACTUAL, {7, 2, 7});
     search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, {5, -12, 2, 1});
-    search_server.AddDocument(3, "ухоженный скворец евгений"s,         DocumentStatus::BANNED, {9});
 
-    const int document_count = search_server.GetDocumentCount();
-    for (int document_id = 0; document_id < document_count; ++document_id) {
-        const auto [words, status] = search_server.MatchDocument("пушистый кот"s, document_id);
-        PrintMatchDocumentResult(document_id, words, status);
+    for (const Document& document : search_server.FindTopDocuments("ухоженный кот"s)) {
+        PrintDocument(document);
     }
 }
