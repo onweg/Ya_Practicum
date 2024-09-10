ee3db2ce3322591a274c5c76f018c948af357397
onweg <Innokentiydiscord@yandex.ru>
2024-09-10 12:29:02 +0300

    спринт 1 9.5.1

--- 01 Основные компоненты C++/01 Спринт 1/09 Шаблоны функций/05 Универсальные функции вывода контейнеров/Задание 1/src/main.cpp
+++ 01 Основные компоненты C++/01 Спринт 1/09 Шаблоны функций/05 Универсальные функции вывода контейнеров/Задание 1/src/main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& container) {
+    for (const auto& element : container) {
+        out << element << " "s;
+    }
+    return out;
+}
+
 int main() {
     const vector<int> ages = {10, 5, 2, 12};
     cout << ages << endl;
