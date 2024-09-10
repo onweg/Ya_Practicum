57ff750465293502f83ac50df4a2ed8247eb22c6
onweg <Innokentiydiscord@yandex.ru>
2024-09-10 15:24:37 +0300

    спринт 1 9.5.5

---
+++ 01 Основные компоненты C++/01 Спринт 1/09 Шаблоны функций/05 Универсальные функции вывода контейнеров/Задание 5/src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+template<typename Container>
+void Print(ostream& out, Container container) {
+    int i = 0, size = static_cast<int>(container.size());
+    for (const auto& element : container) {
+        out << element;
+        if (i + 1 < size) {
+            out << ", ";
+        }
+        ++i;
+    }
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const set<T>& container) {
+    out << "{"s;
+    Print(out, container);
+    out << "}"s;
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& container) {
+    out << "["s;
+    Print(out, container);
+    out << "]"s;
+    return out;
+}
+
+int main() {
+    const vector<int> ages = {10, 5, 2, 12};
+    cout << ages << endl;
+    const set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
+    cout << cats << endl;
+}
