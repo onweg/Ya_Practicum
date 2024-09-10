24e868fd6f858065ac52fa066bbe34f864e59821
onweg <Innokentiydiscord@yandex.ru>
2024-09-10 11:31:57 +0300

    спринт 1 9.4.1

--- 01 Основные компоненты C++/01 Спринт 1/09 Шаблоны функций/04 Как устроены шаблоны/Задание 1/src/main.cpp
+++ 01 Основные компоненты C++/01 Спринт 1/09 Шаблоны функций/04 Как устроены шаблоны/Задание 1/src/main.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+
+template <typename Term>
+// вычислеяет TF-IDF
+auto ComputeTfIdfs(const vector<vector<Term> >& documents, const Term term) {
+    int document_freq = 0;
+    for (const auto& document : documents) 
+        if (find(document.begin(), document.end(), term) != document.end()) 
+            ++document_freq;
+    double idf = document_freq > 0 ? log(double(documents.size()) / document_freq) : 0;
+    vector<double> freqs;
+    for (const auto& document : documents) {
+        if (document.empty()) {
+            freqs.push_back(0.0);
+        } else {
+            freqs.push_back(1.0 * count(document.begin(), document.end(), term) / document.size() * idf);
+        }
+    }
+    return freqs;
+}
+
 int main() {
     const vector<vector<string>> documents = {
         {"белый"s, "кот"s, "и"s, "модный"s, "ошейник"s},
@@ -9,4 +36,5 @@ int main() {
         cout << tf_idf << " "s;
     }
     cout << endl;
+    return 0;
 }
