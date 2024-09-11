2884ddc95b8690f0cfd2b0f97a664b52858cd5e9
onweg <Innokentiydiscord@yandex.ru>
2024-09-11 15:38:47 +0300

    спринт 1 9.6.2

---
+++ 01 Основные компоненты C++/01 Спринт 1/09 Шаблоны функций/06 Функциональные объекты/Задание 2/src/main.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Animal {
+    string name;
+    int age;
+    double weight;
+};
+
+template <typename Container, typename KeyMapper>
+void SortBy(Container& container, KeyMapper key_mapper, bool reverse = false) {
+    sort(container.begin(), container.end(), [key_mapper, reverse](const auto& lhs, const auto& rhs){return reverse ? key_mapper(lhs) > key_mapper(rhs) : key_mapper(lhs) < key_mapper(rhs);});
+}
+
+void PrintNames(const vector<Animal>& animals) {
+    for (const Animal& animal : animals) {
+        cout << animal.name << ' ';
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<Animal> animals = {
+        {"Мурка"s,   10, 5},
+        {"Белка"s,   5,  1.5},
+        {"Георгий"s, 2,  4.5},
+        {"Рюрик"s,   12, 3.1},
+    };
+
+    PrintNames(animals);
+
+    SortBy(animals, [](const Animal& animal) { return animal.name; }, true);
+    PrintNames(animals);
+
+    SortBy(animals, [](const Animal& animal) { return animal.weight; });
+    PrintNames(animals);
+
+    return 0;
+}
