2060438f98a049a5b0be36d4e7215320857979ae
onweg <Innokentiydiscord@yandex.ru>
2024-09-25 15:06:23 +0300

    спринт 2 2.3.1

--- 01 Основные компоненты C++/02 Спринт 2/02 Фреймворк для юнит-тестов/03 Используем макросы и улучшаем фреймворк/Задание 00/src/main.cpp
+++ 01 Основные компоненты C++/02 Спринт 2/02 Фреймворк для юнит-тестов/03 Используем макросы и улучшаем фреймворк/Задание 00/src/main.cpp
@@ -5,17 +5,19 @@
 
 using namespace std;
 
-void AssertImpl(const string& str, const string& file, const int& line, const string& function, const string& hint = ""s) {
-    cout << file << " ("s << line << "): "s << function << ": ASSERT("s << str << ") failed."s;
-    if (!hint.empty()) {
-        cout << " Hint: "s << hint;
+void AssertImpl(bool value, const string& str, const string& file, const int& line, const string& function, const string& hint = ""s) {
+    if (!value){
+        cout << file << " ("s << line << "): "s << function << ": ASSERT("s << str << ") failed."s;
+        if (!hint.empty()) {
+            cout << " Hint: "s << hint;
+        }
+        cout << endl;
     }
-    cout << endl;
 }
 
-#define ASSERT(expr) AssertImpl(#expr, __FILE__, __LINE__, __FUNCTION__, ""s);
+#define ASSERT(expr) AssertImpl((expr), #expr, __FILE__, __LINE__, __FUNCTION__, ""s);
 
-#define ASSERT_HINT(expr, hint) AssertImpl(#expr, __FILE__, __LINE__, __FUNCTION__, hint);
+#define ASSERT_HINT(expr, hint) AssertImpl((expr), #expr, __FILE__, __LINE__, __FUNCTION__, hint);
 
 int main() {
     string hello = "hello"s;
