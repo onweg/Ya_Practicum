b2d9da5d1ec06e21e5840879de68ae3698bca26b
onweg <Innokentiydiscord@yandex.ru>
2024-10-12 01:44:14 +0300

    недоделанный

--- 01 Основные компоненты C++/03 Спринт 3/03 Обработка ошибок. Исключения/06 Обработка ошибок в поисковой системе/Задание 00/src/main.cpp
+++ 01 Основные компоненты C++/03 Спринт 3/03 Обработка ошибок. Исключения/06 Обработка ошибок в поисковой системе/Задание 00/src/main.cpp
@@ -1,17 +1,109 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+vector<string> SplitIntoWords(const string& text) {
+    vector<string> words;
+    string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        } else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+
+    return words;
+}
+
+struct Document {
+    Document() = default;
+
+    Document(int id, double relevance, int rating)
+            : id(id)
+            , relevance(relevance)
+            , rating(rating) {
+    }
+
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+
+template <typename StringContainer> //создает множество непустых строк из запроса (нужно при инициализации)
+set<string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    set<string> non_empty_strings;
+    for (const string& str : strings) {
+        if (!str.empty()) {
+            non_empty_strings.insert(str);
+        }
+    }
+    return non_empty_strings;
+}
+
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+
 class SearchServer {
 public:
     template <typename StringContainer>
-    explicit SearchServer(const StringContainer& stop_words) {
-        ...
+    explicit SearchServer(const StringContainer& stop_words) 
+        : stop_words_(MakeUniqueNonEmptyStrings(stop_words))
+    {
+        /*
+        Конструкторы класса SearchServer должны выбрасывать исключение invalid_argument, если любое из переданных стоп-слов содержит недопустимые символы, то есть символы с кодами от 0 до 31.
+        */
+        for (const auto& word : stop_words_) {
+            if (!IsValidWord(word)) {
+                throw invalid_argument("Некорректные данные для стоп слов!"s);
+            }
+        }
+
     }
 
-    explicit SearchServer(const string& stop_words_text) {
+    explicit SearchServer(const string& stop_words_text) //Уточнить что это?! Можно ли так и корректно ли так?
+    //Походу это конструктор класса через первый конструктор 
+        : SearchServer(SplitIntoWords(stop_words_text))
+    {}
+
+    static bool IsValidWord(const string& word) {
+        // Валидное слово не должно иметь специальных символов
+        return none_of(word.begin(), word.end(), [](char c) {
+            return c >= '\0' && c < ' ';
+        });
     }
 
+    
     void AddDocument(int document_id, const string& document, DocumentStatus status, const vector<int>& ratings) {
-        ...
+        /*
+        Метод должен выбрасывать исключение invalid_argument в следующих ситуациях:
+        Попытка добавить документ с отрицательным id;
+        Попытка добавить документ c id ранее добавленного документа;
+        Наличие недопустимых символов (с кодами от 0 до 31) в тексте добавляемого документа.
+        */
+        if ((document_id < 0) || (documents_.count(document_id) > 0)) {
+            throw invalid_argument("Некорректный id"s);
+        }
     }
 
+    /*
     template <typename DocumentPredicate>
     vector<Document> FindTopDocuments(const string& raw_query, DocumentPredicate document_predicate) const {
         ...
@@ -36,11 +128,24 @@ public:
     tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query, int document_id) const {
         ...
     }
-    ...
+    */
+private:
+    struct DocumentData {
+        int rating;
+        DocumentStatus status;
+    };
+
+    const set<string> stop_words_; //стоп слова
+    map<string, map<int, double>> word_to_document_freqs_; //слово и в каком id документе встречается и сколько весомости имеет
+    map<int, DocumentData> documents_; //id документа и какие данные имеет 
+    vector<int> document_ids_; //id документа до индексу(id документа по добавлению в этот вектор) (странная тема но пусть)
+
+
 };
 
 // ------------ Пример использования ----------------
 
+/*
 void PrintDocument(const Document& document) {
     cout << "{ "s
          << "document_id = "s << document.id << ", "s
@@ -93,9 +198,26 @@ void MatchDocuments(const SearchServer& search_server, const string& query) {
     }
 }
 
-int main() {
-    SearchServer search_server("и в на"s);
+*/
+void TestSearchServer() {
+    try {
+        SearchServer server("и в на"s);
+    } catch(const invalid_argument& e) {
+        cout << "Ошибка: "s << e.what() << endl;
+    }
 
+    try {
+        SearchServer server("и в н\x12а"s);
+    } catch(const invalid_argument& e) {
+        cout << "Ошибка: "s << e.what() << endl;
+    }
+}
+
+int main() {
+    TestSearchServer();
+
+    /*
+    SearchServer search_server("и в на"s);
     AddDocument(search_server, 1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
     AddDocument(search_server, 1, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, {1, 2});
     AddDocument(search_server, -1, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, {1, 2});
@@ -110,4 +232,6 @@ int main() {
     MatchDocuments(search_server, "модный -кот"s);
     MatchDocuments(search_server, "модный --пёс"s);
     MatchDocuments(search_server, "пушистый - хвост"s);
+    */
+    return 0;
 }
