52def7c9eae38efbe3f501fb990c40c193b584aa
onweg <Innokentiydiscord@yandex.ru>
2024-10-15 01:16:58 +0300

    спринт 4 2.1.2.2

--- 01 Основные компоненты C++/04 Спринт 4/02 Алгоритмы поиска/01 Итераторы/02 Понятие итератора/Задание 01/src/main.cpp
+++ 01 Основные компоненты C++/04 Спринт 4/02 Алгоритмы поиска/01 Итераторы/02 Понятие итератора/Задание 01/src/main.cpp
@@ -12,7 +12,7 @@ struct Lang {
 int main() {
     vector<Lang> langs = {{"Python"s, 29}, {"Java"s, 24}, {"C#"s, 20}, {"Ruby"s, 25}, {"C++"s, 37}};
     // Выведите первый язык, начинающийся на J, используя алгоритм find_if
-    auto lang = find_if(langs.begin(), langs.end(), [](Lang lang){
+    auto lang = find_if(langs.begin(), langs.end(), [](const Lang& lang){
         if (!lang.name.empty()) return lang.name[0] == 'J';
         return false;
     });
