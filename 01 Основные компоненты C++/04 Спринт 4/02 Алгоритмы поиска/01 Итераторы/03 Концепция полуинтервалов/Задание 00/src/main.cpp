a99c4a63be1fc5786802bb37da8d74d8eb08ccab
onweg <Innokentiydiscord@yandex.ru>
2024-10-15 01:33:35 +0300

    спринт 4 2.1.3.0

--- 01 Основные компоненты C++/04 Спринт 4/02 Алгоритмы поиска/01 Итераторы/03 Концепция полуинтервалов/Задание 00/src/main.cpp
+++ 01 Основные компоненты C++/04 Спринт 4/02 Алгоритмы поиска/01 Итераторы/03 Концепция полуинтервалов/Задание 00/src/main.cpp
@@ -1,1 +1,18 @@
 //Заготовки кода здесь нет — вы легко справитесь без неё. А вот решение не забудьте сохранить — оно вам понадобится в следующем задании.
+#include <iostream>
+#include <set>
+using namespace std;
+
+void PrintRange(auto it_begin, auto it_end) {
+    while (it_begin != it_end) {
+        cout << *it_begin;
+        it_begin++;
+        if (it_begin != it_end) cout << " "s;
+    }
+    cout << endl;
+} 
+
+int main() {
+    set<int> test = {1, 1, 1, 2, 3, 4, 5, 5};
+    PrintRange(test.begin(), test.end());
+}
