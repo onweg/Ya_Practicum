41be1a3dc24595cd085341c983aa2b614e682f0c
onweg <Innokentiydiscord@yandex.ru>
2024-10-14 22:16:28 +0300

    недоделанный

--- 01 Основные компоненты C++/03 Спринт 3/03 Обработка ошибок. Исключения/06 Обработка ошибок в поисковой системе/Задание 00/src/main.cpp
+++ 01 Основные компоненты C++/03 Спринт 3/03 Обработка ошибок. Исключения/06 Обработка ошибок в поисковой системе/Задание 00/src/main.cpp
@@ -71,7 +71,7 @@ public:
         Конструкторы класса SearchServer должны выбрасывать исключение invalid_argument, если любое из переданных стоп-слов содержит недопустимые символы, то есть символы с кодами от 0 до 31.
         */
         for (const auto& word : stop_words_) {
-            if (!IsValidWord(word)) {
+            if (!IsValidString(word)) {
                 throw invalid_argument("Некорректные данные для стоп слов!"s);
             }
         }
@@ -83,13 +83,25 @@ public:
         : SearchServer(SplitIntoWords(stop_words_text))
     {}
 
-    static bool IsValidWord(const string& word) {
+    static bool IsValidString(const string& word) {
         // Валидное слово не должно иметь специальных символов
         return none_of(word.begin(), word.end(), [](char c) {
             return c >= '\0' && c < ' ';
         });
     }
 
+    static bool CorrectUseMinusSignInText(const string& text) {
+        size_t i = 0, size = text.size();
+        for (; i < size; i++) {
+            if (text[i] == '-') {
+                if (i + 1 == size) return false // если символ последний
+                if (text[i + 1] == ' ' or text[i + 1] == '-') return false // если после символа пробел или еще один минус
+            }
+        }
+        return true;
+    }
+
+    
     
     void AddDocument(int document_id, const string& document, DocumentStatus status, const vector<int>& ratings) {
         /*
@@ -101,34 +113,103 @@ public:
         if ((document_id < 0) || (documents_.count(document_id) > 0)) {
             throw invalid_argument("Некорректный id"s);
         }
+        if (!IsValidString(document)) {
+            throw invalid_argument("Некоректный запрос"s);
+        }
+
+        const vector<string> words = SplitIntoWordsNoStop(document);
+        const double inv_word_count = 1.0 / words.size();
+        for (const string& word : words) {
+            word_to_document_freqs_[word][document_id] += inv_word_count;
+        }
+        documents_.emplace(document_id,
+            DocumentData{
+                ComputeAverageRating(ratings),
+                status
+            });
     }
 
-    /*
+    
     template <typename DocumentPredicate>
     vector<Document> FindTopDocuments(const string& raw_query, DocumentPredicate document_predicate) const {
-        ...
+        /*
+        Методы FindDocument должны выбрасывать исключение invalid_argument в следующих ситуациях:
+        В словах поискового запроса есть недопустимые символы с кодами от 0 до 31;
+        Наличие более чем одного минуса перед словами, которых не должно быть в искомых документах, например, пушистый --кот. В середине слов минусы разрешаются, например: иван-чай.
+        Отсутствие текста после символа «минус»: в поисковом запросе: "пушистый -".
+        */
+        if (!IsValidString(raw_query) or !CorrectUseMinusSignInText(raw_query)) {
+            throw invalid_argument("Некорректное использование запроса"s);
+        }
+
+        const Query query = ParseQuery(raw_query);
+        auto matched_documents = FindAllDocuments(query, document_predicate);
+        
+        sort(matched_documents.begin(), matched_documents.end(),
+             [](const Document& lhs, const Document& rhs) {
+                if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                    return lhs.rating > rhs.rating;
+                } else {
+                    return lhs.relevance > rhs.relevance;
+                }
+             });
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+        return matched_documents;
     }
 
+
     vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const {
-        ...
+        return FindTopDocuments(raw_query, [status](int document_id, DocumentStatus document_status, int rating) { return document_status == status; });
     }
 
     vector<Document> FindTopDocuments(const string& raw_query) const {
-        ...
+        return FindTopDocuments(raw_query, DocumentStatus::ACTUAL);
     }
 
     int GetDocumentCount() const {
-        ...
+        return documents_.size();
     }
-
+/*
     int GetDocumentId(int index) const {
         ...
     }
-
+*/
     tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query, int document_id) const {
-        ...
+        /*
+        Метод MatchDocument должен выбрасывать исключение invalid_argument в следующих ситуациях:
+        В словах поискового запроса есть недопустимые символы с кодами от 0 до 31;
+        Наличие более чем одного минуса перед словами, которых не должно быть в искомых документах, например, пушистый --кот. В середине слов минусы разрешаются, например: иван-чай.
+        Отсутствие текста после символа «минус»: в поисковом запросе: "пушистый -".
+        */
+
+        if (!IsValidString(raw_query) or !CorrectUseMinusSignInText(raw_query)) {
+            throw invalid_argument("Некорректное использование запроса"s);
+        }
+
+        const Query query = ParseQuery(raw_query);
+        vector<string> matched_words;
+        for (const string& word : query.plus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            if (word_to_document_freqs_.at(word).count(document_id)) {
+                matched_words.push_back(word);
+            }
+        }
+        for (const string& word : query.minus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            if (word_to_document_freqs_.at(word).count(document_id)) {
+                matched_words.clear();
+                break;
+            }
+        }
+        return {matched_words, documents_.at(document_id).status};
     }
-    */
+    
 private:
     struct DocumentData {
         int rating;
@@ -140,7 +221,109 @@ private:
     map<int, DocumentData> documents_; //id документа и какие данные имеет 
     vector<int> document_ids_; //id документа до индексу(id документа по добавлению в этот вектор) (странная тема но пусть)
 
-
+    bool IsStopWord(const string& word) const {
+        return stop_words_.count(word) > 0;
+    }
+    
+    vector<string> SplitIntoWordsNoStop(const string& text) const {
+        vector<string> words;
+        for (const string& word : SplitIntoWords(text)) {
+            if (!IsStopWord(word)) {
+                words.push_back(word);
+            }
+        }
+        return words;
+    }
+    
+    static int ComputeAverageRating(const vector<int>& ratings) {
+        if (ratings.empty()) {
+            return 0;
+        }
+        int rating_sum = 0;
+        for (const int rating : ratings) {
+            rating_sum += rating;
+        }
+        return rating_sum / static_cast<int>(ratings.size());
+    }
+    
+    struct QueryWord {
+        string data;
+        bool is_minus;
+        bool is_stop;
+    };
+    
+    QueryWord ParseQueryWord(string text) const {
+        bool is_minus = false;
+        // Word shouldn't be empty
+        if (text[0] == '-') {
+            is_minus = true;
+            text = text.substr(1);
+        }
+        return {
+            text,
+            is_minus,
+            IsStopWord(text)
+        };
+    }
+    
+    struct Query {
+        set<string> plus_words;
+        set<string> minus_words;
+    };
+    
+    Query ParseQuery(const string& text) const {
+        Query query;
+        for (const string& word : SplitIntoWords(text)) {
+            const QueryWord query_word = ParseQueryWord(word);
+            if (!query_word.is_stop) {
+                if (query_word.is_minus) {
+                    query.minus_words.insert(query_word.data);
+                } else {
+                    query.plus_words.insert(query_word.data);
+                }
+            }
+        }
+        return query;
+    }
+    
+    // Existence required
+    double ComputeWordInverseDocumentFreq(const string& word) const {
+        return log(GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
+    }
+    template <typename DocumentPredicate>
+    vector<Document> FindAllDocuments(const Query& query, DocumentPredicate document_predicate) const {
+        map<int, double> document_to_relevance;
+        for (const string& word : query.plus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
+            for (const auto [document_id, term_freq] : word_to_document_freqs_.at(word)) {
+                const auto& document_data = documents_.at(document_id);
+                if (document_predicate(document_id, document_data.status, document_data.rating)) {
+                    document_to_relevance[document_id] += term_freq * inverse_document_freq;
+                }
+            }
+        }
+        
+        for (const string& word : query.minus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            for (const auto [document_id, _] : word_to_document_freqs_.at(word)) {
+                document_to_relevance.erase(document_id);
+            }
+        }
+        vector<Document> matched_documents;
+        for (const auto [document_id, relevance] : document_to_relevance) {
+            matched_documents.push_back({
+                document_id,
+                relevance,
+                documents_.at(document_id).rating
+            });
+        }
+        return matched_documents;
+    }
 };
 
 // ------------ Пример использования ----------------
@@ -199,7 +382,8 @@ void MatchDocuments(const SearchServer& search_server, const string& query) {
 }
 
 */
-void TestSearchServer() {
+
+void TestInitSearchServer() {
     try {
         SearchServer server("и в на"s);
     } catch(const invalid_argument& e) {
@@ -210,7 +394,52 @@ void TestSearchServer() {
         SearchServer server("и в н\x12а"s);
     } catch(const invalid_argument& e) {
         cout << "Ошибка: "s << e.what() << endl;
+    }  
+}
+
+void TestSearchServerAddDocument() {
+    try {
+        SearchServer server("и в на"s);
+        server.AddDocument(0, "кошки мышки"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    } catch(const invalid_argument& e) {
+        cout << "Ошибка: "s << e.what() << endl;
     }
+
+    try {
+        SearchServer server("и в на"s);
+        server.AddDocument(0, "кош\x12ки мышки"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    } catch(const invalid_argument& e) {
+        cout << "Ошибка: "s << e.what() << endl;
+    }
+
+    try {
+        SearchServer server("и в на"s);
+        server.AddDocument(-1, "кошки мышки"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    } catch(const invalid_argument& e) {
+        cout << "Ошибка: "s << e.what() << endl;
+    }
+
+    try {
+        SearchServer server("и в на"s);
+        server.AddDocument(0, "кошки мышки"s, DocumentStatus::ACTUAL, {1, 2, 3});
+        server.AddDocument(0, "кошки мышки"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    } catch(const invalid_argument& e) {
+        cout << "Ошибка: "s << e.what() << endl;
+    }
+}
+
+void TestSearchServerFindTopDocuments() {
+    try {
+        SearchServer server("и в на"s);
+        server.AddDocument(0, "кошки мышки"s, DocumentStatus::ACTUAL, {1, 2, 3});
+        server.FindTopDocuments()
+    } catch(const invalid_argument& e) {
+        cout << "Ошибка: "s << e.what() << endl;
+    }
+}
+
+void TestSearchServer() {
+    TestSearchServerAddDocument();
 }
 
 int main() {
@@ -235,3 +464,8 @@ int main() {
     */
     return 0;
 }
+
+
+// найти актуальную версию search server
+// протестировать findtopdocuments 
+// протестировать матчинг дкументов
