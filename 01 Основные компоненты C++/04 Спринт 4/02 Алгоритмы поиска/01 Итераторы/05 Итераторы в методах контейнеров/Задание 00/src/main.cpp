0ce3825fd0ed0662071bd4ec4ece912527e3e356
onweg <Innokentiydiscord@yandex.ru>
2024-10-16 22:08:07 +0300

    спринт 4 2.1.5.0

---
+++ 01 Основные компоненты C++/04 Спринт 4/02 Алгоритмы поиска/01 Итераторы/05 Итераторы в методах контейнеров/Задание 00/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+template <typename It>
+void PrintRange(It range_begin, It range_end) {
+    for (auto it = range_begin; it != range_end; ++it) {
+        cout << *it << " "s;
+    }
+    cout << endl;
+}
+
+template <typename Container, typename Iterator>
+void EraseAndPrint(Container& container, Iterator iterator) {
+    PrintRange(container.begin(), iterator);
+    PrintRange(iterator + 1, container.end());
+    container.erase(iterator);
+}
+
+int main() {
+    vector<string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
+    EraseAndPrint(langs, langs.begin());
+}
