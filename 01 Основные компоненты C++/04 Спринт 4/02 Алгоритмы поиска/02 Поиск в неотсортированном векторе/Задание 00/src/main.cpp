ef06d282c8422864e63bfe3364f2f6cb93cd80d6
onweg <Innokentiydiscord@yandex.ru>
2024-10-19 18:10:00 +0300

    спринт 4 2.2.0

--- 01 Основные компоненты C++/04 Спринт 4/02 Алгоритмы поиска/02 Поиск в неотсортированном векторе/Задание 00/src/main.cpp
+++ 01 Основные компоненты C++/04 Спринт 4/02 Алгоритмы поиска/02 Поиск в неотсортированном векторе/Задание 00/src/main.cpp
@@ -1,3 +1,20 @@
-void PrintSpacesPositions(string& str) {
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void PrintSpacesPositions(const string& str) {
     // напишите реализацию
+    char space = ' ';
+    auto it = find(str.begin(), str.end(), space);
+    while (it != str.end()) {
+        cout << distance(str.begin(), it) << endl;
+        it = find(next(it), str.end(), space);
+    }
+}
+
+int main() {
+    string str = "He said: one and one and one is three"s;
+    PrintSpacesPositions(str);
+    return 0;
 }
