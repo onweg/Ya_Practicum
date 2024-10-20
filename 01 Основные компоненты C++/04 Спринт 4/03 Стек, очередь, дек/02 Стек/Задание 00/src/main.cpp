bdc1214d37c7981e7ca7261e93c7ac3708ecb2cb
onweg <Innokentiydiscord@yandex.ru>
2024-10-20 19:22:05 +0300

    спринт 4 3.2.0

--- 01 Основные компоненты C++/04 Спринт 4/03 Стек, очередь, дек/02 Стек/Задание 00/src/main.cpp
+++ 01 Основные компоненты C++/04 Спринт 4/03 Стек, очередь, дек/02 Стек/Задание 00/src/main.cpp
@@ -16,26 +16,63 @@ class Stack {
 public:
     void Push(const Type& element) {
         // напишите реализацию
+        elements_.push_back(element);
     }
     void Pop() {
         // напишите реализацию
+        if (elements_.empty()) {
+            throw out_of_range("Невозможно удалить элемент в пустом стеке"s);
+        }
+        elements_.pop_back();
     }
     const Type& Peek() const {
         // напишите реализацию
+        cout << "const Type&\n";
+        if (elements_.empty()) {
+            throw out_of_range("Невозможно взять элемент в пустом стеке"s);
+        }
+        return elements_[elements_.size() - 1];
     }
     Type& Peek() {
         // напишите реализацию
+        cout << "Type&\n";
+        if (elements_.empty()) {
+            throw out_of_range("Невозможно взять элемент в пустом стеке"s);
+        }
+        return elements_[elements_.size() - 1];
     }
     void Print() const {
         // напишите реализацию
+        for (const auto element : elements_) {
+            cout << element << " "s;
+        }
+        cout << endl;
     }
     uint64_t Size() const {
         // напишите реализацию
+        return elements_.size();
     }
     bool IsEmpty() const {
         // напишите реализацию
+        return elements_.empty();
     }
 
 private:
     vector<Type> elements_;
 };
+
+int main() {
+    Stack<int> stack;
+    for (uint32_t i = 0; i < 10; ++i) {
+        stack.Push(i);
+        stack.Print();
+    }
+    const int& top = stack.Peek();
+    stack.Print();
+    while (!stack.IsEmpty()) {
+        stack.Pop();
+        stack.Print();
+    }
+
+    return 0;
+}
