917810e5cee762dfcaab40c3f56322251b60b916
onweg <Innokentiydiscord@yandex.ru>
2024-10-21 01:29:48 +0300

    спринт 4 4.8.0

--- 01 Основные компоненты C++/04 Спринт 4/04 Распределение кода по файлам/08 Шаблоны в многофайловых проектах/Задание 00/src/accountant.h
+++ 01 Основные компоненты C++/04 Спринт 4/04 Распределение кода по файлам/08 Шаблоны в многофайловых проектах/Задание 00/src/accountant.h
@@ -4,10 +4,19 @@
 
 #include <utility>
 
+using namespace std;
+
 class Accountant {
 public:
     template <class BuildingObject>
-    double CalcPaintNeeded(const BuildingObject& building_object) const;
+    double CalcPaintNeeded(const BuildingObject& building_object) const{
+    pair<double, double> sizes = building_object.GetSizes();
+    return CalcSquare(sizes.first, sizes.second) * 0.4;
+}
     template <class BuildingObject>
-    double CalcBricksNeeded(const BuildingObject& building_object) const;
+    double CalcBricksNeeded(const BuildingObject& building_object) const{
+    pair<double, double> sizes = building_object.GetSizes();
+    return CalcSquare(sizes.first, sizes.second) * 5;
+}
 };
+
