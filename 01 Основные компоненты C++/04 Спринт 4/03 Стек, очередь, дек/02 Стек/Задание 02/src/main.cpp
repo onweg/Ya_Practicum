1c21552e33b1678e224258aede9bd21ff16ad985
onweg <Innokentiydiscord@yandex.ru>
2024-10-20 20:38:00 +0300

    спринт 4 3.2.2

--- 01 Основные компоненты C++/04 Спринт 4/03 Стек, очередь, дек/02 Стек/Задание 02/src/main.cpp
+++ 01 Основные компоненты C++/04 Спринт 4/03 Стек, очередь, дек/02 Стек/Задание 02/src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
+#include <algorithm>
+#include <random>
 
 using namespace std;
 
@@ -40,14 +43,60 @@ private:
     vector<Type> elements_;
 };
 
-int main() {
-    Stack<int> stack;
-    for (uint32_t i = 0; i < 10; ++i) {
-        stack.Push(i);
-        stack.Print();
+template <typename Type>
+class SortedStack {
+public:
+    void Push(const Type& element) {
+    // напишите реализацию метода
+        Stack<int> tmp_stack;
+        while (!elements_.IsEmpty() && elements_.Peek() < element) {
+            tmp_stack.Push(elements_.Peek());
+            elements_.Pop();
+        }
+        elements_.Push(element);
+        while (!tmp_stack.IsEmpty()) {
+            elements_.Push(tmp_stack.Peek());
+            tmp_stack.Pop();
+        }
     }
-    while (!stack.IsEmpty()) {
-        stack.Pop();
+    void Pop() {
+    // напишите реализацию метода
+    elements_.Pop();
+    }
+    const Type& Peek() const {
+    return elements_.Peek();
+    }
+    Type& Peek() {
+    return elements_.Peek();
+    }
+    void Print() const {
+    elements_.Print();
+    }
+    uint64_t Size() const {
+    return elements_.Size();
+    }
+    bool IsEmpty() const {
+    return elements_.IsEmpty();
+    }
+private:
+    Stack<Type> elements_;
+};
+
+int main() {
+    SortedStack<int> stack;
+    vector<int> values(5);
+
+    // заполняем вектор для тестирования нашего стека
+    iota(values.begin(), values.end(), 1);
+    // перемешиваем значения
+    std::random_device rd;   // Источник случайных чисел
+    std::mt19937 g(rd());    // Генератор на основе Mersenne Twister
+    shuffle(values.begin(), values.end(), g);
+
+    // заполняем стек и проверяем, что сортировка сохраняется после каждой вставки
+    for (int i = 0; i < 5; ++i) {
+        cout << "Вставляемый элемент = "s << values[i] << endl;
+        stack.Push(values[i]);
         stack.Print();
     }
 }
