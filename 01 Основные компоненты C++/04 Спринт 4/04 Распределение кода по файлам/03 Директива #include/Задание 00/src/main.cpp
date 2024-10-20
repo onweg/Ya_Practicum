e62b2e9df2c52393a69b7b9120140662bf92efb1
onweg <Innokentiydiscord@yandex.ru>
2024-10-20 22:43:24 +0300

    спринт 4.4.3

--- 01 Основные компоненты C++/04 Спринт 4/04 Распределение кода по файлам/03 Директива #include/Задание 00/src/main.cpp
+++ 01 Основные компоненты C++/04 Спринт 4/04 Распределение кода по файлам/03 Директива #include/Задание 00/src/main.cpp
@@ -1,6 +1,10 @@
+#include "wall.h"
 #include "builder.h"
+#include <iostream>
 
 int main() {
     Builder tom;
     // Попросите Тома посчитать кирпичи для стены 3.5 х 2.45
+    Wall wall(3.5, 2.5);
+    std::cout << tom.CalcBricksNeeded(wall);
 }
